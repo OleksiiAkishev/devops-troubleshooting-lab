b0b144d9c65511f4a053260e02b0a81744b859ac
Oleksii Akishev <QLB3@it.eurofinseu.com>
2026-05-04 15:11:23 +0200

    refactor: inodes check logic

--- linux/scripts/system_health.sh
+++ linux/scripts/system_health.sh
@@ -4,9 +4,11 @@ white_list_inodes="tmpfs devtmpfs proc sysfs cgroup2 cgroup autofs binfmt_misc s
 df -i -T | awk -v inodes_usage_threshold="$1" -v white_list_inodes="$white_list_inodes" '
 NR > 1 { 
     gsub("%", "", $6);
-    if (index(white_list_inodes, $2) != 0) {
-        print "INFO:", "filesystem", $1, "of type", $2, "has", $5, "inodes used:", $6"%"
-    } else if ($6 >= inodes_usage_threshold) {
-        print "WARNING:", "filesystem", $1, "of type", $2, "has", $5, "inodes used:", $6"%"
+    if ($6 >= inodes_usage_threshold) {
+        if (index(white_list_inodes, $2) != 0) {
+            print "WARNING:", "filesystem", $1, "of type", $2, "has", $5, "inodes used:", $6"%"
+        } else {
+            print "CRITICAL:", "filesystem", $1, "of type", $2, "has", $5, "inodes used:", $6"%"
+        }
     }
 }'
