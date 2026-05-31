ce276df2880b6ff7fe21159718eaa340f044ffe0
Oleksii Akishev <akishev.oleksii@gmail.com>
2026-05-31 20:15:43 +0200

    implement: finish week1_setup script

--- week1_setup.sh
+++ week1_setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-# Remove any log files inside tmp 
-# Example: *_logs.txt
-rm /tmp/*_logs.txt /tmp/*.log
+set -e
 
+# Ask user to run as root
+if ["$EUID" -ne 0]; then
+   echo "Please run as root"
+   exit 1
+fi
 
+# Clean up any traces from the log files (files, archives)
+rm /tmp/*_logs.txt /tmp/*.log /tmp/*.tar.bz2 /tmp/all_system_setuid_files.txt
 
+# Delete group and beloged users to it
+userdel -r sarah
+userdel -r alex
+groupdel sysadmins
 
+# Delete shared directory and all files inside it
+rm -r /tmp/shared
 
+echo "Cleanup successfully done"
